e228dc7f3f76e365fc89965f5ae21fde08986ec1
jgwentworth92 <95505687+jgwentworth92@users.noreply.github.com>
2023-03-22 18:43:51 -0400

    wrote 4 lighthouse tests 2 for performance and 2 for accessability

--- tests/lighthouse.test.js
+++ tests/lighthouse.test.js
@@ -20,3 +20,64 @@ test('Lighthouse audit home page', async ({ page }) => {
   const performanceScore = lhr.categories.performance.score * 100;
   expect(performanceScore).toBeGreaterThanOrEqual(60);
 });
+
+
+test('Lighthouse audit home page for accessibility', async ({ page }) => {
+    await page.goto('http://localhost:3000');
+  
+    const url = page.url();
+    const reportPath = 'lighthouse-report.json';
+    const command = `lighthouse ${url} --output json --output-path ${reportPath} --only-categories=accessibility`;
+  
+    try {
+      execSync(command);
+    } catch (error) {
+      console.error('Error running Lighthouse:', error);
+    }
+  
+    const lhr = JSON.parse(readFileSync(reportPath));
+  
+    const accessibilityScore = lhr.categories.accessibility.score * 100;
+    expect(accessibilityScore).toBeGreaterThanOrEqual(90);
+  });
+  
+  test('Lighthouse audit content page for accessibility', async ({ page }) => {
+    await page.goto('http://localhost:3000/content.html');
+  
+    const url = page.url();
+    const reportPath = 'lighthouse-report.json';
+    const command = `lighthouse ${url} --output json --output-path ${reportPath} --only-categories=accessibility`;
+  
+    try {
+      execSync(command);
+    } catch (error) {
+      console.error('Error running Lighthouse:', error);
+    }
+  
+    const lhr = JSON.parse(readFileSync(reportPath));
+  
+    const accessibilityScore = lhr.categories.accessibility.score * 100;
+    expect(accessibilityScore).toBeGreaterThanOrEqual(90);
+  });
+
+
+test('Lighthouse audit content page', async ({ page }) => {
+    await page.goto('http://localhost:3000/content.html');
+  
+    const url = page.url();
+    const reportPath = 'lighthouse-report.json';
+    const command = `lighthouse ${url} --output json --output-path ${reportPath} --only-categories=performance`;
+  
+    try {
+      execSync(command);
+    } catch (error) {
+      console.error('Error running Lighthouse:', error);
+    }
+  
+    const lhr = JSON.parse(readFileSync(reportPath));
+  
+    const performanceScore = lhr.categories.performance.score * 100;
+    expect(performanceScore).toBeGreaterThanOrEqual(70);
+  });
+
+
