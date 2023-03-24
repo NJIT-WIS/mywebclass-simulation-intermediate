27c4d6558501f521122625a16c03971bc91c5bb0
Jad229 <jad229@njit.edu>
2023-03-24 02:21:28 -0400

    FEATURE: updated gitignore

--- nextjs-tailwind/.next/static/chunks/webpack.js
+++ nextjs-tailwind/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "08780e785ea554b3"; }
+/******/ 		__webpack_require__.h = function() { return "d8fd11babc994231"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
