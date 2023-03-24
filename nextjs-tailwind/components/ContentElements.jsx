db4518ec5b0536b60f0168790ec087fba817deae
Jad229 <76543236+Jad229@users.noreply.github.com>
2023-03-24 11:08:11 -0400

    FEATURE: broke down content page components

---
+++ nextjs-tailwind/components/ContentElements.jsx
@@ -0,0 +1,229 @@
+function ContentElements() {
+  return (
+    <div>
+      <main class="basis-3/4 col-span-9 md:col-span-9 lg:col-span-9 px-4">
+        <section class="mt-24" id="title">
+          <h1 class="text-4xl font-bold border-b">Article Title</h1>
+          <div class="text-gray-500 mb-3">
+            By <a href="#">Author Name</a>
+            <span class="ml-3">Last Updated: January 1, 2022</span>
+            <span class="ml-3" id="readingTime">
+              0
+            </span>
+          </div>
+        </section>
+        <section class="my-11" id="section2">
+          <h2 class="text-3xl font-semi">Section 2</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+            auctor augue massa, ac maximus nibh dapibus vel. Nulla luctus, magna
+            at pulvinar tincidunt, massa lacus iaculis risus, nec efficitur
+            risus ex ac lectus. Quisque id lacus non velit aliquet ultrices.
+            Morbi vel arcu varius, commodo nulla eu, luctus est. Donec
+            efficitur, sapi
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+            auctor augue massa, ac maximus nibh dapibus vel. Nulla luctus, magna
+            at pulvinar tincidunt, massa lacus iaculis risus, nec efficitur
+            risus ex ac lectus. Quisque id lacus non velit aliquet ultrices.
+            Morbi vel arcu varius, commodo nulla eu, luctus est. Donec
+            efficitur, sapi
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+            auctor augue massa, ac maximus nibh dapibus vel. Nulla luctus, magna
+            at pulvinar tincidunt, massa lacus iaculis risus, nec efficitur
+            risus ex ac lectus. Quisque id lacus non velit aliquet ultrices.
+            Morbi vel arcu varius, commodo nulla eu, luctus est. Donec
+            efficitur, sapi
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+            auctor augue massa, ac maximus nibh dapibus vel. Nulla luctus, magna
+            at pulvinar tincidunt, massa lacus iaculis risus, nec efficitur
+            risus ex ac lectus. Quisque id lacus non velit aliquet ultrices.
+            Morbi vel arcu varius, commodo nulla eu, luctus est. Donec
+            efficitur, sapi
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+            auctor augue massa, ac maximus nibh dapibus vel. Nulla luctus, magna
+            at pulvinar tincidunt, massa lacus iaculis risus, nec efficitur
+            risus ex ac lectus. Quisque id lacus non velit aliquet ultrices.
+            Morbi vel arcu varius, commodo nulla eu, luctus est. Donec
+            efficitur, sapi
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+            auctor augue massa, ac maximus nibh dapibus vel. Nulla luctus, magna
+            at pulvinar tincidunt, massa lacus iaculis risus, nec efficitur
+            risus ex ac lectus. Quisque id lacus non velit aliquet ultrices.
+            Morbi vel arcu varius, commodo nulla eu, luctus est. Donec
+            efficitur, sapi
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+            auctor augue massa, ac maximus nibh dapibus vel. Nulla luctus, magna
+            at pulvinar tincidunt, massa lacus iaculis risus, nec efficitur
+            risus ex ac lectus. Quisque id lacus non velit aliquet ultrices.
+            Morbi vel arcu varius, commodo nulla eu, luctus est. Donec
+            efficitur, sapi
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+            auctor augue massa, ac maximus nibh dapibus vel. Nulla luctus, magna
+            at pulvinar tincidunt, massa lacus iaculis risus, nec efficitur
+            risus ex ac lectus. Quisque id lacus non velit aliquet ultrices.
+            Morbi vel arcu varius, commodo nulla eu, luctus est. Donec
+            efficitur, sapi
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+            auctor augue massa, ac maximus nibh dapibus vel. Nulla luctus, magna
+            at pulvinar tincidunt, massa lacus iaculis risus, nec efficitur
+            risus ex ac lectus. Quisque id lacus non velit aliquet ultrices.
+            Morbi vel arcu varius, commodo nulla eu, luctus est. Donec
+            efficitur, sapi
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+            auctor augue massa, ac maximus nibh dapibus vel. Nulla luctus, magna
+            at pulvinar tincidunt, massa lacus iaculis risus, nec efficitur
+            risus ex ac lectus. Quisque id lacus non velit aliquet ultrices.
+            Morbi vel arcu varius, commodo nulla eu, luctus est. Donec
+            efficitur, sapi
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+            auctor augue massa, ac maximus nibh dapibus vel. Nulla luctus, magna
+            at pulvinar tincidunt, massa lacus iaculis risus, nec efficitur
+            risus ex ac lectus. Quisque id lacus non velit aliquet ultrices.
+            Morbi vel arcu varius, commodo nulla eu, luctus est. Donec
+            efficitur, sapi
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+            auctor augue massa, ac maximus nibh dapibus vel. Nulla luctus, magna
+            at pulvinar tincidunt, massa lacus iaculis risus, nec efficitur
+            risus ex ac lectus. Quisque id lacus non velit aliquet ultrices.
+            Morbi vel arcu varius, commodo nulla eu, luctus est. Donec
+            efficitur, sapi
+          </p>
+        </section>
+        <section class="my-11" id="section3">
+          <h2 class="text-3xl font-semi">Section 3</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+            auctor augue massa, ac maximus nibh dapibus vel. Nulla luctus, magna
+            at pulvinar tincidunt, massa lacus iaculis risus, nec efficitur
+            risus ex ac lectus. Quisque id lacus non velit aliquet ultrices.
+            Morbi vel arcu varius, commodo nulla eu, luctus est. Donec
+            efficitur, sapi
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+            auctor augue massa, ac maximus nibh dapibus vel. Nulla luctus, magna
+            at pulvinar tincidunt, massa lacus iaculis risus, nec efficitur
+            risus ex ac lectus. Quisque id lacus non velit aliquet ultrices.
+            Morbi vel arcu varius, commodo nulla eu, luctus est. Donec
+            efficitur, sapi
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+            auctor augue massa, ac maximus nibh dapibus vel. Nulla luctus, magna
+            at pulvinar tincidunt, massa lacus iaculis risus, nec efficitur
+            risus ex ac lectus. Quisque id lacus non velit aliquet ultrices.
+            Morbi vel arcu varius, commodo nulla eu, luctus est. Donec
+            efficitur, sapi
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+            auctor augue massa, ac maximus nibh dapibus vel. Nulla luctus, magna
+            at pulvinar tincidunt, massa lacus iaculis risus, nec efficitur
+            risus ex ac lectus. Quisque id lacus non velit aliquet ultrices.
+            Morbi vel arcu varius, commodo nulla eu, luctus est. Donec
+            efficitur, sapi
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+            auctor augue massa, ac maximus nibh dapibus vel. Nulla luctus, magna
+            at pulvinar tincidunt, massa lacus iaculis risus, nec efficitur
+            risus ex ac lectus. Quisque id lacus non velit aliquet ultrices.
+            Morbi vel arcu varius, commodo nulla eu, luctus est. Donec
+            efficitur, sapi
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+            auctor augue massa, ac maximus nibh dapibus vel. Nulla luctus, magna
+            at pulvinar tincidunt, massa lacus iaculis risus, nec efficitur
+            risus ex ac lectus. Quisque id lacus non velit aliquet ultrices.
+            Morbi vel arcu varius, commodo nulla eu, luctus est. Donec
+            efficitur, sapi
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+            auctor augue massa, ac maximus nibh dapibus vel. Nulla luctus, magna
+            at pulvinar tincidunt, massa lacus iaculis risus, nec efficitur
+            risus ex ac lectus. Quisque id lacus non velit aliquet ultrices.
+            Morbi vel arcu varius, commodo nulla eu, luctus est. Donec
+            efficitur, sapi
+          </p>
+        </section>
+        <section class="my-11" id="section4">
+          <h2 class="text-3xl font-semi">Section 4</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+            auctor augue massa, ac maximus nibh dapibus vel. Nulla luctus, magna
+            at pulvinar tincidunt, massa lacus iaculis risus, nec efficitur
+            risus ex ac lectus. Quisque id lacus non velit aliquet ultrices.
+            Morbi vel arcu varius, commodo nulla eu, luctus est. Donec
+            efficitur, sapi
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+            auctor augue massa, ac maximus nibh dapibus vel. Nulla luctus, magna
+            at pulvinar tincidunt, massa lacus iaculis risus, nec efficitur
+            risus ex ac lectus. Quisque id lacus non velit aliquet ultrices.
+            Morbi vel arcu varius, commodo nulla eu, luctus est. Donec
+            efficitur, sapi
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+            auctor augue massa, ac maximus nibh dapibus vel. Nulla luctus, magna
+            at pulvinar tincidunt, massa lacus iaculis risus, nec efficitur
+            risus ex ac lectus. Quisque id lacus non velit aliquet ultrices.
+            Morbi vel arcu varius, commodo nulla eu, luctus est. Donec
+            efficitur, sapi
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+            auctor augue massa, ac maximus nibh dapibus vel. Nulla luctus, magna
+            at pulvinar tincidunt, massa lacus iaculis risus, nec efficitur
+            risus ex ac lectus. Quisque id lacus non velit aliquet ultrices.
+            Morbi vel arcu varius, commodo nulla eu, luctus est. Donec
+            efficitur, sapi
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+            auctor augue massa, ac maximus nibh dapibus vel. Nulla luctus, magna
+            at pulvinar tincidunt, massa lacus iaculis risus, nec efficitur
+            risus ex ac lectus. Quisque id lacus non velit aliquet ultrices.
+            Morbi vel arcu varius, commodo nulla eu, luctus est. Donec
+            efficitur, sapi
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+            auctor augue massa, ac maximus nibh dapibus vel. Nulla luctus, magna
+            at pulvinar tincidunt, massa lacus iaculis risus, nec efficitur
+            risus ex ac lectus. Quisque id lacus non velit aliquet ultrices.
+            Morbi vel arcu varius, commodo nulla eu, luctus est. Donec
+            efficitur, sapi
+          </p>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default ContentElements;
