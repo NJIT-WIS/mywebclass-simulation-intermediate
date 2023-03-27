8f7a63e3f53f3b4a2369407970075a14fea75aba
Jad229 <jad229@njit.edu>
2023-03-26 22:31:46 -0400

    CHANGES: moved test into tailwind folder

---
+++ nextjs-tailwind/tests/performance.spec.js
@@ -0,0 +1,31 @@
+const { test, expect } = require("@playwright/test");
+
+test("Measures page metrics", async ({ page }) => {
+  const lighthouse = require("lighthouse/core");
+
+  const { lhr } = await lighthouse(page, {
+    port: new URL(page.url()).port,
+    plugins: [loadLighthousePlugin("lighthouse-plugin-field-performance")],
+    settings: {
+      formFactor: "desktop",
+      throttling: {
+        rttMs: 40,
+        throughputKbps: 10_000,
+        cpuSlowdownMultiplier: 1,
+      },
+    },
+  });
+
+  // Access Lighthouse results.
+  const { categories } = lhr;
+  const {
+    performance,
+    accessibility,
+    "best-practices": bestPractices,
+  } = categories;
+
+  // Assertions.
+  expect(performance.score).toBeGreaterThan(0.9);
+  expect(accessibility.score).toBeGreaterThan(0.9);
+  expect(bestPractices.score).toBeGreaterThan(0.9);
+});
