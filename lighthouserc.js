6f66b5540804f9c1185fee28809018f4b1176a4e
Jad229 <jad229@njit.edu>
2023-03-26 23:31:43 -0400

    FIX: updated lhci config file

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,5 +1,8 @@
 module.exports = {
   ci: {
+    collect: {
+      staticDistDir: "./nextjs-tailwind/out", // Update this with the directory where your static site files are located
+    },
     upload: {
       target: "temporary-public-storage",
     },
