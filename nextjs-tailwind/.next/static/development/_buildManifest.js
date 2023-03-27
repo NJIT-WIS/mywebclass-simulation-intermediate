e1acf09cf91336a5975c946f6f1e5923b12518a0
Jad229 <jad229@njit.edu>
2023-03-26 21:52:31 -0400

    FEATURE: updated yaml file to use test tailwindcss branch

--- nextjs-tailwind/.next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/content":["static\u002Fchunks\u002Fpages\u002Fcontent.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fcontent"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
